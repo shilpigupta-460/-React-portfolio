05043595d0c843039fd6196f01c46490038bbd08
shilpigupta-460 <shilpigupta460@gmail.com>
2022-09-21 18:27:16 -0700

    waiting to deploy

---
+++ src/routes/Contact.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Navbar from "../../../../React_Portfolio/portfolio/src/components/Navbar/Navbar"
+import HeroImage from "../../../../React_Portfolio/portfolio/src/components/HeroImage/HeroImage"
+import Footer from "../components/Footer/Footer"
+import Img from "../assets/photo3.jpg"
+import Form from "../components/Form/Form"
+ import Heading from '../../../../React_Portfolio/portfolio/src/components/Heading'
+function Contact() {
+    return (
+        <div>
+            <div>
+                <Navbar />
+                <HeroImage  image={Img} heading={ <Heading message={ `
+ React developer passionate to develope seamless user experience with responsive design.` } /> } style={{ height: '30vh' }} /> 
+
+                <Form />
+                <Footer />
+            </div>
+        </div>
+    )
+}
+
+export default Contact
